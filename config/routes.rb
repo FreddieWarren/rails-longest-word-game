4f314b5bcf4bc1ef825c5c5b9271ac6f1aaf8edc
freddie warren <fredswow@live.co.uk>
2019-11-11 16:59:34 +0000

    longest word game

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'games#new'
+  get 'game', to: 'games#new'
+  get 'result', to: 'games#score'
 end
